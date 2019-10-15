80f153e54a15fd13077f59cf349660da820811a8
uchoudh <umair.choudhary@shopify.com>
2019-10-15 14:20:47 -0400

    Refactor

--- src/devtools.ts
+++ src/devtools.ts
@@ -1,57 +1,77 @@
 import * as d3 from 'd3';
 import * as flamegraph from 'd3-flame-graph';
 import 'd3-flame-graph/dist/d3-flamegraph.css';
+// import './panel.css';
+// import './main.css';
 
-chrome.devtools.panels.create('Shopify', '', './devtools.html');
+chrome.devtools.panels.create('Shopify', '', './devtools.html', function() {
+  return displayFlameGraph();
+});
 
-function cleanLiquidProfileData(entries) {
+function formatLiquidProfileData(entries) {
   return entries.map(function(entry) {
     return {
       name: `${entry.partial} ${entry.code} (line#${entry.line_number})`,
       value: entry.total_time,
-      children: cleanLiquidProfileData(entry.children),
+      children: formatLiquidProfileData(entry.children),
     };
   });
 }
 
-chrome.devtools.inspectedWindow.eval('window.location.href', function(
-  result: string,
+function getProfileURL(): Promise<string> {
+  return new Promise(resolve => {
+    chrome.devtools.inspectedWindow.eval('window.location.href', function(
+      result: string,
+    ) {
+      resolve(result);
+    });
+  });
+}
+
+async function getProfileData() {
+  const url = new URL(await getProfileURL());
+  url.searchParams.set('profile_liquid', 'true');
+  const response = await fetch(url.href);
+  const html = await response.text();
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, 'text/html');
+  if (doc === null) {
+    return;
+  }
+  const profileData = JSON.parse(
+    doc.querySelector('#liquidProfileData')!.innerHTML,
+  );
+  // eslint-disable-next-line consistent-return
+  return cleanProfileData(profileData);
+}
+
+function cleanProfileData(
+  profileData: {name: any; value: any; children: any} | null,
 ) {
-  const url = result;
-  const request = new XMLHttpRequest();
-  request.responseType = 'document';
-  request.open('GET', `${url}?profile_liquid`, true);
-  request.onload = function() {
-    if (request.readyState === 4) {
-      if (request.status === 200) {
-        let data;
-        const scriptTag = request.responseXML!.querySelector(
-          '#liquidProfileData',
-        );
-        if (scriptTag !== null) {
-          data = JSON.parse(scriptTag.innerHTML);
-        }
-        const cleanData = {
-          name: data.name,
-          value: data.value,
-          children: cleanLiquidProfileData(data.children),
-        };
+  let cleanData;
 
-        const flameGraph = flamegraph.flamegraph().width(600);
+  if (profileData !== null) {
+    cleanData = {
+      name: profileData.name,
+      value: profileData.value,
+      children: formatLiquidProfileData(profileData.children),
+    };
+  }
+  return cleanData;
+}
 
-        const details = document.getElementById('details');
-        flameGraph.setDetailsElement(details);
+async function displayFlameGraph() {
+  const profile = await getProfileData();
 
-        d3.select('#chart')
-          .datum(cleanData)
-          .call(flameGraph);
-      } else {
-        console.error(request.statusText);
-      }
-    }
-  };
-  request.onerror = function() {
-    console.error(request.statusText);
-  };
-  request.send(null);
-});
+  const flameGraph = flamegraph
+    .flamegraph()
+    .inverted(true)
+    .cellHeight(20);
+
+  const details = document.getElementById('details');
+  flameGraph.setDetailsElement(details);
+
+  d3.select('#chart')
+    .datum(profile)
+    .call(flameGraph);
+}
