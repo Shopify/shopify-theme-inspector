e392afa58fb48e7a4a525e0d0b85db668c09edf9
Helen Lin <helen.lin@shopify.com>
2020-01-24 15:41:12 -0800

    fix test

--- test/test-helpers.ts
+++ test/test-helpers.ts
@@ -24,8 +24,8 @@ export function setDevtoolsEval(page: any) {
       window.chrome.devtools.inspectedWindow.eval = function(value, cb){
         if (value === "typeof window.Shopify === 'object'") {
           return cb(true)
-        } else if (value === "document.location.href") {
-          return cb('https://shop1.myshopify.io')
+        }  else if (value === "Shopify.shop") {
+          return cb('shop1.myshopify.io')
         }
       };
       window.chrome.devtools.panels.create = () => {};
